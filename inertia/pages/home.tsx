ffa3e53e1cc6eb17eee2d7df14c14ad3b979322f
diabyml <diaby223.ml@gmail.com>
2024-06-30 12:58:00 +0000

    configured shadcnui

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,17 +1,21 @@
 import { Head } from '@inertiajs/react'
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
 
 export default function Home(props: { version: number }) {
   return (
     <>
       <Head title="Homepage" />
 
-      <div className="container">
-        <div className="title">AdonisJS {props.version} x Inertia x React</div>
+      <div className="container max-w-3xl mx-auto bg-gray-300">
+        <div className="text-blue-500 font-bold">AdonisJS {props.version} x Inertia x React</div>
 
         <span>
           Learn more about AdonisJS and Inertia.js by visiting the{' '}
           <a href="https://docs.adonisjs.com/guides/inertia">AdonisJS documentation</a>.
         </span>
+        <Input placeholder="Enter name" />
+        <Button>Hello, world</Button>
       </div>
     </>
   )
